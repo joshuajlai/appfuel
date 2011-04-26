4c6e78ca61461c4ce118ca7aca8e9fb8754a1650
Robert Scott-Buccluech <rsb.code@gmail.com>
2011-04-26 07:44:58 -0700

    adding changes from bag to dictionary

--- src/test/lib/Test/Appfuel/StdLib/Data/BagTest.php
+++ src/test/lib/Test/Appfuel/StdLib/Data/BagTest.php
@@ -11,30 +11,30 @@
 namespace Test\Appfuel\Stdlib\Data;
 
 use Test\AfTestCase	as ParentTestCase,
-	Appfuel\Stdlib\BagInterface,
-	Appfuel\Stdlib\Data\Bag,
+	Appfuel\Stdlib\DictionaryInterface,
+	Appfuel\Stdlib\Data\Dictionary,
 	StdClass;
 
 /**
- * A data bag is a general data structure used to manage an associative array. 
+ * A data dictionary is a general data structure used to manage an associative array. 
  * The primary focus of this structure is to remove the need to check if the 
  * keys exists. It also provides the ability to return a default value when
  * the key you are looking for does not exist. 
  */
-class BagTest extends ParentTestCase
+class DictionaryTest extends ParentTestCase
 {
 	/**
 	 * System Under Test
-	 * @var Appfuel\Stdlib\Data\Bag
+	 * @var Appfuel\Stdlib\Data\Dictionary
 	 */
-	protected $bag = NULL;
+	protected $dictionary = NULL;
 
 	/**
 	 * @return null
 	 */
 	public function setUp()
 	{
-		$this->bag = new Bag();
+		$this->dictionary = new Dictionary();
 	}
 
 	/**
@@ -42,7 +42,7 @@ class BagTest extends ParentTestCase
 	 */
 	public function tearDown()
 	{
-		unset($this->bag);
+		unset($this->dictionary);
 	}
 
 	/**
@@ -53,37 +53,37 @@ class BagTest extends ParentTestCase
 	public function testAddGetExistsCount()
 	{
 		
-		$this->assertEquals(0, $this->bag->count());
+		$this->assertEquals(0, $this->dictionary->count());
 		
 		/* add a string */
-		$this->assertFalse($this->bag->exists('label_1'));
-		$result = $this->bag->add('label_1', 'this is a string');
-		$this->assertSame($this->bag, $result);
-		$this->assertEquals(1, $this->bag->count());
-		$this->assertTrue($this->bag->exists('label_1'));
-		$this->assertEquals('this is a string', $this->bag->get('label_1'));
+		$this->assertFalse($this->dictionary->exists('label_1'));
+		$result = $this->dictionary->add('label_1', 'this is a string');
+		$this->assertSame($this->dictionary, $result);
+		$this->assertEquals(1, $this->dictionary->count());
+		$this->assertTrue($this->dictionary->exists('label_1'));
+		$this->assertEquals('this is a string', $this->dictionary->get('label_1'));
 
 		/* add an integer */		
-		$this->assertFalse($this->bag->exists('label_2'));
-		$result = $this->bag->add('label_2', 12345);
-		$this->assertTrue($this->bag->exists('label_2'));
-		$this->assertEquals(2, $this->bag->count());
-		$this->assertEquals(12345, $this->bag->get('label_2'));
+		$this->assertFalse($this->dictionary->exists('label_2'));
+		$result = $this->dictionary->add('label_2', 12345);
+		$this->assertTrue($this->dictionary->exists('label_2'));
+		$this->assertEquals(2, $this->dictionary->count());
+		$this->assertEquals(12345, $this->dictionary->get('label_2'));
 
 		/* add an array */
-		$this->assertFalse($this->bag->exists('label_3'));
-		$result = $this->bag->add('label_3', array(1,2,3,4,5));
-		$this->assertTrue($this->bag->exists('label_3'));
-		$this->assertEquals(3, $this->bag->count());
-		$this->assertEquals(array(1,2,3,4,5), $this->bag->get('label_3'));
+		$this->assertFalse($this->dictionary->exists('label_3'));
+		$result = $this->dictionary->add('label_3', array(1,2,3,4,5));
+		$this->assertTrue($this->dictionary->exists('label_3'));
+		$this->assertEquals(3, $this->dictionary->count());
+		$this->assertEquals(array(1,2,3,4,5), $this->dictionary->get('label_3'));
 
 		/* add an object */
-		$this->assertFalse($this->bag->exists('label_4'));
+		$this->assertFalse($this->dictionary->exists('label_4'));
 		$obj = new StdClass();
-		$result = $this->bag->add('label_4', $obj);
-		$this->assertTrue($this->bag->exists('label_4'));
-		$this->assertEquals(4, $this->bag->count());
-		$this->assertSame($obj, $this->bag->get('label_4'));
+		$result = $this->dictionary->add('label_4', $obj);
+		$this->assertTrue($this->dictionary->exists('label_4'));
+		$this->assertEquals(4, $this->dictionary->count());
+		$this->assertSame($obj, $this->dictionary->get('label_4'));
 	}
 
 	/**
@@ -94,22 +94,22 @@ class BagTest extends ParentTestCase
 	 */
 	public function testAddOverride()
 	{
-		$this->assertEquals(0, $this->bag->count());
+		$this->assertEquals(0, $this->dictionary->count());
 		$value_1 = 12345;
 		$value_2 = 'this is a string';
 		$label   = 'label_1';
 
 		/* normal add */
-		$this->bag->add($label, $value_1);
-		$this->assertEquals(1, $this->bag->count());
-		$this->assertTrue($this->bag->exists($label));
-		$this->assertEquals($value_1, $this->bag->get($label));	
+		$this->dictionary->add($label, $value_1);
+		$this->assertEquals(1, $this->dictionary->count());
+		$this->assertTrue($this->dictionary->exists($label));
+		$this->assertEquals($value_1, $this->dictionary->get($label));	
 
 		/* override label with new value */
-		$this->bag->add($label, $value_2);
-		$this->assertEquals(1, $this->bag->count());
-		$this->assertTrue($this->bag->exists($label));
-		$this->assertEquals($value_2, $this->bag->get($label));	
+		$this->dictionary->add($label, $value_2);
+		$this->assertEquals(1, $this->dictionary->count());
+		$this->assertTrue($this->dictionary->exists($label));
+		$this->assertEquals($value_2, $this->dictionary->get($label));	
 	}
 
 	/**
@@ -119,16 +119,16 @@ class BagTest extends ParentTestCase
 	 */
 	public function testGetDefaultValues()
 	{
-		$this->assertEquals(0, $this->bag->count());
-		$this->assertFalse($this->bag->exists('nothing'));
+		$this->assertEquals(0, $this->dictionary->count());
+		$this->assertFalse($this->dictionary->exists('nothing'));
 
-		$this->assertNull($this->bag->get('nothing'));
-		$this->assertEquals(array(), $this->bag->get('nothing', array()));
-		$this->assertEquals('test', $this->bag->get('nothing', 'test'));
-		$this->assertEquals(12345, $this->bag->get('nothing', 12345));
+		$this->assertNull($this->dictionary->get('nothing'));
+		$this->assertEquals(array(), $this->dictionary->get('nothing', array()));
+		$this->assertEquals('test', $this->dictionary->get('nothing', 'test'));
+		$this->assertEquals(12345, $this->dictionary->get('nothing', 12345));
 
 		$obj = new StdClass();
-		$this->assertEquals($obj, $this->bag->get('nothing', $obj));
+		$this->assertEquals($obj, $this->dictionary->get('nothing', $obj));
 	}
 
 	/**
@@ -136,17 +136,17 @@ class BagTest extends ParentTestCase
 	 */
 	public function testGetAllLoad()
 	{
-		$this->assertEquals(0, $this->bag->count());
+		$this->assertEquals(0, $this->dictionary->count());
 		$data = array(
 			'label_1' => 'value_1',
 			'label_2' => 12345,
 			'label_3' => array(1,2,3,4)
 		);
 
-		$result = $this->bag->load($data);
-		$this->assertSame($this->bag, $result);
-		$this->assertEquals(count($data), $this->bag->count());
-		$this->assertEquals($data, $this->bag->getAll());
+		$result = $this->dictionary->load($data);
+		$this->assertSame($this->dictionary, $result);
+		$this->assertEquals(count($data), $this->dictionary->count());
+		$this->assertEquals($data, $this->dictionary->getAll());
 	}
 
 }
