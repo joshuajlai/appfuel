77a94ee5435e694be4c8389032c8bc9438127758
Robert Scott-Buccluech <rsb.code@gmail.com>
2011-05-12 09:33:55 -0700

    adding html document test and changes to the doc

--- lib/Appfuel/App/View/Html/Document.php
+++ lib/Appfuel/App/View/Html/Document.php
@@ -14,7 +14,14 @@ use Appfuel\Framework\Exception,
 	Appfuel\Framework\FileInterface,
 	Appfuel\Framework\View\TemplateInterface,
 	Appfuel\App\View\Template,
-	Appfuel\View\Html\Element\Title;
+	Appfuel\View\Html\Element\Tag,
+	Appfuel\View\Html\Element\Title,
+	Appfuel\View\Html\Element\Base,
+	Appfuel\View\Html\Element\Meta\Charset,
+	Appfuel\View\Html\Element\Script,
+	Appfuel\View\Html\Element\Style,
+	Appfuel\View\Html\Element\Link,
+	Appfuel\View\Html\Element\Meta\Tag as MetaTag;
 
 /**
  * Html document template. Used to manage the html document. This template 
@@ -30,19 +37,194 @@ class Document extends Template implements TemplateInterface
 
 	/**
 	 * Specifies the default url and default target for all links on the page
+	 * @var Base
+	 */
+	protected $base = null;
+
+	/**
+	 * Charset meta tag. Can only have one of these
+	 * @var Charset
+	 */
+	protected $charset = null;
+
+	/**
+	 * A list of meta tags located in the head of the html document
 	 * @var array
 	 */
-	protected $base = array(
-		'href'    = null,
-		'target'  = null
+	protected $meta = array();
+
+	/**
+	 * List of link tags located in the head of the html document
+	 * @var array
+	 */
+	protected $css = array();
+	
+	/**
+	 * Flag used to determine if css is enabled
+	 * @var bool
+	 */
+	protected $isCss = true;
+
+	/**
+	 * Inline css located in the head of the document
+	 * @var Style
+	 */
+	protected $inlineCss = null;
+
+	/**
+	 * Flag used to determine if inline styles are enabled
+	 * @var bool
+	 */
+	protected $isInlineCss = false;
+
+	/**
+	 * List of scripts located either in the head or end of the body
+	 * @var array
+	 */
+	protected $js = array(
+		'head' => array(),
+		'body' => array(),
 	);
 
-	public function __construct()
+	/**
+	 * Flag used to determine if javascript is enabled. This would include
+	 * inline js (head,body) and script tags (head, body)
+	 * @var bool
+	 */
+	protected $isJs = true;
+
+	/**
+	 * Flag used to determine if either head or body inlineJs is enabled
+	 * @var bool
+	 */
+	protected $isInlineJs = true;
+
+	/**
+	 * Document behavior located in the head of the document
+	 * @var Script
+	 */
+	protected $inlineJs = array(
+		'head' => null,
+		'body' => null
+	);
+
+	/**
+	 * Add the template file used to render the markup and assign 
+	 * any name/value pairs into scope
+	 *
+	 * @param	array	scope data
+	 * @return	Document
+	 */
+	public function __construct(array $data = array())
 	{
-		$this->addFile('markup', 'doc/standard.phtml')
-			 ->addFile('inline-js', 'doc/init.pjs');
+		parent::__construct($data);
+		$this->addFile('markup', 'doc/standard.phtml');
 	}
 
+	/**
+	 * Build the template file into a string
+	 * 
+	 * @return string
+	 */
+	public function build()
+	{
+		$title = $this->getTitleTag();
+		if (! $title instanceof Title) {
+			$title = new Title('Appfuel Default Html Document');
+		}
+		$this->assign('html-head-title', $title->build());
+
+		$base = $this->getBase();
+		if ($base instanceof Base) {
+			$this->assign('html-head-base', $base->build());
+		}
+	
+		$charset = $this->getCharset();
+		if ($charset instanceof Charset) {
+			$this->assign('html-head-charset', $charset->build());
+		}
+
+		$metaTags = $this->getMeta();
+		if (! empty($metaTags)) {
+			$tags = array();
+			foreach ($metaTags as $tag) {
+				$tags[] = $tag->build();
+			}
+			$this->assign('html-head-meta', $tags);
+		}
+
+		if ($this->isCss()) {
+			$cssTags = $this->getCssLinks();
+			
+			$tags = array();    
+			foreach ($cssTags as $tag) {
+				$tags[] = $tag->build();
+			}
+			$this->assign('html-head-links', $tags);
+
+			if ($this->isInlineCss()) {
+				$style = $this->getInlineCss();
+				if ($style instanceof Style) {
+					$this->assign('html-head-inline-style', $style->build());
+				}
+			}
+		}
+
+		if ($this->isJs()) {
+			/* refactor this block into a single loop */
+			$jsTags = $this->getJsScripts('head');
+			if (! empty($jsTags)) {
+				$tags = array();
+				foreach ($tags as $tag) {
+					$tags[] = $tag->build();
+				}
+				$this->assign('html-head-scripts', $tags);
+			}
+
+			$jsTags = $this->getJsScripts('body');
+			if (! empty($jsTags)) {
+				$tags = array();
+				foreach ($tags as $tag) {
+					$tags[] = $tag->build();
+				}
+				$this->assign('html-body-scripts', $tags);
+			}
+
+			if ($this->isInlineJs()) {
+				$script = $this->getInlineJs('head');
+				if ($script instanceof Script) {
+					$this->assign('html-head-inline-script', $script->build());
+				}
+
+				$script = $this->getInlineJs('body');
+				if ($script instanceof Script) {
+					$this->assign('html-body-inline-script', $script->build());
+				}
+			}
+		}
+
+		/*
+		 * The layout hold all the html content so there is not much
+		 * sense building without it. This will produce an empty html
+		 * document
+		 */
+		if (! $this->isLayout()) {
+			return $this->buildFile('markup');
+		}
+
+		$layout  = $this->getLayout();
+		$this->assign('layout-content', $layout->build()); 
+
+		if ($this->fileExists('inline-css')) {
+			$css = $this->buildFile('inline-css');
+			$this->assign('inline-css', $css);
+		}
+
+		$inlineJs = '';
+		if ($this->fileExists('inline-js')) {
+		
+		}
+	}
 	/**
 	 * @return Title
 	 */
@@ -79,40 +261,237 @@ class Document extends Template implements TemplateInterface
 		return $this->base;
 	}
 
-
-	public function build()
+	/**
+	 * @param	Base	$tag
+	 * @return	Document
+	 */
+	public function setCharset(Charset $tag)
 	{
-		$title = $this->getTitleTag();
-		if (! $title instanceof Title) {
-			$title = new Title('Appfuel Default Html Document');
-		}
-		$this->assign('html-head-title', $title->build());
+		$this->charset = $tag;
+		return $this;
+	}
 
-		$base = $this->getBase();
-		if ($base instanceof Base) {
-			$this->assign('html-head-base', $base->build());
-		}
+	/**
+	 * @return	Base
+	 */
+	public function getCharset()
+	{
+		return $this->charset;
+	}
 
-		/*
-		 * The layout hold all the html content so there is not much
-		 * sense building without it. This will produce an empty html
-		 * document
-		 */
-		if (! $this->isLayout()) {
-			return $this->buildFile('markup');
-		}
-
-		$layout  = $this->getLayout();
-		$this->assign('layout-content', $layout->build()); 
-
-		if ($this->fileExists('inline-css')) {
-			$css = $this->buildFile('inline-css');
-			$this->assign('inline-css', $css);
+	/**
+	 * Adds all meta tags accept the charset. Only one charset is allowed
+	 * and it is set through setCharset.
+	 *
+	 * @param	MetaTag $tag
+	 * @return	Document
+	 */
+	public function addMeta(MetaTag $tag)
+	{
+		if ($tag->attributeExists('charset')) {
+			return $this;
 		}
 
-		$inlineJs = '';
-		if ($this->fileExists('inline-js')) {
-		
+		$this->meta[] = $tag;
+		return $this;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getMeta()
+	{
+		return $this->meta;
+	}
+
+	/**
+	 * @param	Link	$tag
+	 * @return	Document
+	 */
+	public function addCssLink(Link $tag)
+	{
+		$this->css[] = $tag;
+		return $this;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getCssLinks()
+	{
+		return $this->css;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function enableCss()
+	{
+		$this->isCss = true;
+		return $this;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function disableCss()
+	{
+		$this->isCss = false;
+		return $this;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isCss()
+	{
+		return $this->isCss;
+	}
+
+	/**
+	 * @param	Style	$tag
+	 * @return	Document
+	 */
+	public function setInlineCss(Style $tag)
+	{
+		$this->inlineCss = $tag;
+		return $this;
+	}
+
+	/**
+	 * @return Style
+	 */
+	public function getInlineCss()
+	{
+		return $this->inlineCss;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function enableInlineCss()
+	{
+		$this->isInlineCss = true;
+		return $this;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function disableInlineCss()
+	{
+		$this->isInlineCss = false;
+		return $this;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isInlineCss()
+	{
+		return $this->isInlineCss;
+	}
+
+	/**
+	 * @param	Script	$tag
+	 * @return	Document
+	 */
+	public function addJsScript(Script $tag, $location = 'body')
+	{
+		if (! in_array($location, array('body', 'head'))) {
+			return $this;
 		}
+
+		$this->js[$location][] = $tag;
+		return $this;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getJsScripts($location = 'body')
+	{		
+		if (! in_array($location, array('body', 'head'))) {
+			return array();
+		}
+		return $this->js[$location];
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function enableJs()
+	{
+		$this->isJs = true;
+		return $this;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function disableJs()
+	{
+		$this->isJs = false;
+		return $this;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isJs()
+	{
+		return $this->isJs;
+	}
+
+	/**
+	 * @param	Script	$tag
+	 * @return	Document
+	 */
+	public function setInlineJs(Script $tag, $location = 'body')
+	{
+		if (! in_array($location, array('body', 'head'))) {
+			return $this;
+		}
+
+		$this->inlineJs[$location] = $tag;
+		return $this;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getInlineJs($location)
+	{		
+		if (! in_array($location, array('body', 'head'))) {
+			return null;
+		}
+		return $this->inlineJs[$location];
+	}
+
+
+	/**
+	 * @return bool
+	 */
+	public function enableInlineJs()
+	{
+		$this->isInlineJs = true;
+		return $this;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function disableInlineJs()
+	{
+		$this->isInlineJs = false;
+		return $this;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isInlineJs()
+	{
+		return $this->isInlineJs;
 	}
 }
---
+++ test/lib/Test/Appfuel/App/View/Html/DocumentTest.php
@@ -0,0 +1,452 @@
+<?php
+/**
+ * Appfuel
+ * PHP 5.3+ object oriented MVC framework supporting domain driven design. 
+ *
+ * @package     Appfuel
+ * @author      Robert Scott-Buccleuch <rsb.code@gmail.com.com>
+ * @copyright   2009-2010 Robert Scott-Buccleuch <rsb.code@gmail.com>
+ * @license     http://www.apache.org/licenses/LICENSE-2.0
+ */
+namespace Test\Appfuel\App\View;
+
+use Test\AfTestCase as ParentTestCase,
+	Appfuel\App\View\Html\Document,
+	Appfuel\View\Html\Element\Meta\Tag as MetaTag,
+	StdClass;
+
+/**
+ * The html document is a template file that controller the html head, css 
+ * link tags, css style tag, javascript script files and content and body tag
+ * It does not manage the content inside the body tag. That is left to the 
+ * layout.
+ */
+class DocumentTest extends ParentTestCase
+{
+	/**
+	 * System under test
+	 * @var Template
+	 */
+	protected $doc = null;
+
+	/**
+	 * @return null
+	 */
+	public function setUp()
+	{
+		$this->doc = new Document();
+	}
+
+	/**
+	 * @return null
+	 */
+	public function tearDown()
+	{
+		unset($this->doc);
+	}
+
+	/**
+	 * @return null
+	 */	
+	public function testConstructor()
+	{
+		$this->assertInstanceOf(
+			'Appfuel\App\View\Template',
+			$this->doc,
+			'The html doc is also a template'
+		);
+
+		$this->assertInstanceOf(
+			'Appfuel\App\View\Data',
+			$this->doc,
+			'The html doc must extend the view data class'
+		);
+
+		$this->assertInstanceOf(
+			'Appfuel\Stdlib\Data\Dictionary',
+			$this->doc,
+			'The json doc is also a dictionary'
+		);
+	}
+
+	/**
+	 * @return null
+	 */
+	public function testGetSetTitle()
+	{
+		/* initial value is null */
+		$this->assertNull($this->doc->getTitle());
+		
+		$title = $this->getMockBuilder('\Appfuel\View\Html\Element\Title')
+					  ->disableOriginalConstructor()
+					  ->getMock();
+
+		$this->assertSame(
+			$this->doc,
+			$this->doc->setTitle($title),
+			'uses fluent interface'
+		);
+
+		$this->assertSame($title, $this->doc->getTitle());
+	}
+
+	/**
+	 * @return null
+	 */
+	public function testGetSetBase()
+	{
+		/* initial value is null */
+		$this->assertNull($this->doc->getBase());
+		
+		$base = $this->getMockBuilder('\Appfuel\View\Html\Element\Base')
+					  ->disableOriginalConstructor()
+					  ->getMock();
+
+		$this->assertSame(
+			$this->doc,
+			$this->doc->setBase($base),
+			'uses fluent interface'
+		);
+
+		$this->assertSame($base, $this->doc->getBase());
+	}
+
+	/**
+	 * @return null
+	 */
+	public function testGetSetCharset()
+	{
+		/* initial value is null */
+		$this->assertNull($this->doc->getCharset());
+		
+		$class = '\Appfuel\View\Html\Element\Meta\Charset';	
+		$tag   = $this->getMockBuilder($class)
+					  ->disableOriginalConstructor()
+					  ->getMock();
+
+		$this->assertSame(
+			$this->doc,
+			$this->doc->setCharset($tag),
+			'uses fluent interface'
+		);
+
+		$this->assertSame($tag, $this->doc->getCharset());
+	}
+
+	/**
+	 * @return null
+	 */
+	public function testAddGetMeta()
+	{
+		/* initial value is null */
+		$result = $this->doc->getMeta();
+		$this->assertInternalType('array', $result);
+		$this->assertEmpty($result);
+		
+		$tag = new MetaTag();
+		
+		$this->assertSame(
+			$this->doc,
+			$this->doc->addMeta($tag),
+			'uses a fluent interface'
+		);
+
+		$expected = array($tag);
+		$result = $this->doc->getMeta();
+		$this->assertEquals($expected, $result);
+
+		$tag2 = new MetaTag();
+		$this->doc->addMeta($tag2);
+
+		$expected = array($tag, $tag2);
+		$result = $this->doc->getMeta();
+		$this->assertEquals($expected, $result);
+
+		/* prove charsets are not added */
+		$tag3 = new MetaTag();
+		$tag3->addAttribute('charset', 'UTF-8');
+	
+		$this->assertSame(
+			$this->doc,
+			$this->doc->addMeta($tag3),
+			'uses a fluent interface'
+		);
+
+		/* 
+		 * note that expected is still the same array with just two tags
+		 * and not 3
+		 */
+		$result = $this->doc->getMeta();
+		$this->assertEquals($expected, $result);
+	}
+
+	/**
+	 * @return null
+	 */
+	public function testAddGetCss()
+	{
+		/* initial value is null */
+		$result = $this->doc->getCssLinks();
+		$this->assertInternalType('array', $result);
+		$this->assertEmpty($result);
+			
+		$class = '\Appfuel\View\Html\Element\Link';
+		$tag   = $this->getMockBuilder($class)
+					  ->disableOriginalConstructor()
+					  ->getMock();
+		
+		$this->assertSame(
+			$this->doc,
+			$this->doc->addCssLink($tag),
+			'uses a fluent interface'
+		);
+
+		$expected = array($tag);
+		$result = $this->doc->getCssLinks();
+		$this->assertEquals($expected, $result);
+
+		$tag2 = $this->getMockBuilder($class)
+					 ->disableOriginalConstructor()
+					 ->getMock();
+
+		$this->doc->addCssLink($tag2);
+
+		$expected = array($tag, $tag2);
+		$result = $this->doc->getCssLinks();
+		$this->assertEquals($expected, $result);
+	}
+
+	/**
+	 * Used to determine if styles are enabled or disabled for the html doc.
+	 *
+	 * @return null
+	 */
+	public function testIsEnableDisableCss()
+	{
+		/* default setting is true */
+		$this->assertTrue($this->doc->isCss());
+
+		$this->assertSame(
+			$this->doc,
+			$this->doc->disableCss(),
+			'uses fluent interface'
+		);
+		$this->assertFalse($this->doc->isCss());
+	
+		$this->assertSame(
+			$this->doc,
+			$this->doc->enableCss(),
+			'uses fluent interface'
+		);
+
+		$this->assertTrue($this->doc->isCss());
+	}
+
+	/**
+	 * @return null
+	 */
+	public function testGetSetInlineStyle()
+	{
+		/* initial value is null */
+		$this->assertNull($this->doc->getInlineCss());
+		
+		$tag = $this->getMockBuilder('\Appfuel\View\Html\Element\Style')
+					  ->disableOriginalConstructor()
+					  ->getMock();
+
+		$this->assertSame(
+			$this->doc,
+			$this->doc->setInlineCss($tag),
+			'uses fluent interface'
+		);
+
+		$this->assertSame($tag, $this->doc->getInlineCss());
+	}
+
+	/**
+	 * Used to determine if inline styles are enabled or disabled for the 
+	 * html doc.
+	 *
+	 * @return null
+	 */
+	public function testIsEnableDisableInlineCss()
+	{
+		/* default setting is false */
+		$this->assertFalse($this->doc->isInlineCss());
+
+		$this->assertSame(
+			$this->doc,
+			$this->doc->enableInlineCss(),
+			'uses fluent interface'
+		);
+		$this->assertTrue($this->doc->isInlineCss());
+	
+		$this->assertSame(
+			$this->doc,
+			$this->doc->disableInlineCss(),
+			'uses fluent interface'
+		);
+
+		$this->assertFalse($this->doc->isInlineCss());
+	}
+
+	/**
+	 * Javascript files can be stored in one of two locations: in the html head
+	 * or in the body addJsScript takes two parameter, the tag and the location
+	 * we will be testing that add does in fact store script in one of those 
+	 * locations
+	 *
+	 * @return null
+	 */
+	public function testGetAddJsScript()
+	{
+		/* default values are empty arrays */
+		$this->assertEquals(array(), $this->doc->getJsScripts('head'));
+		$this->assertEquals(array(), $this->doc->getJsScripts('body'));
+
+		$tag = $this->getMockBuilder('\Appfuel\View\Html\Element\Script')
+					  ->disableOriginalConstructor()
+					  ->getMock();
+
+		$this->assertSame(
+			$this->doc,
+			$this->doc->addJsScript($tag, 'head'),
+			'uses fluent interface'
+		);
+
+		$expectedHead = array($tag);
+		$this->assertEquals($expectedHead, $this->doc->getJsScripts('head'));
+		$this->assertEquals(array(), $this->doc->getJsScripts('body'));
+
+		$tag2 = $this->getMockBuilder('\Appfuel\View\Html\Element\Script')
+					 ->disableOriginalConstructor()
+					 ->getMock();
+
+		$this->doc->addJsScript($tag2, 'head');
+		$expectedHead = array($tag, $tag2);
+		$this->assertEquals($expectedHead, $this->doc->getJsScripts('head'));
+		$this->assertEquals(array(), $this->doc->getJsScripts('body'));
+
+		$tag3 = $this->getMockBuilder('\Appfuel\View\Html\Element\Script')
+					 ->disableOriginalConstructor()
+					 ->getMock();
+
+
+		$this->doc->addJsScript($tag3, 'body');
+		$expectedBody = array($tag3);
+		$this->assertEquals($expectedHead, $this->doc->getJsScripts('head'));
+		$this->assertEquals($expectedBody, $this->doc->getJsScripts('body'));
+
+		$tag4 = $this->getMockBuilder('\Appfuel\View\Html\Element\Script')
+					 ->disableOriginalConstructor()
+					 ->getMock();
+
+		$this->doc->addJsScript($tag4, 'body');
+		$expectedBody = array($tag3, $tag4);
+		$this->assertEquals($expectedHead, $this->doc->getJsScripts('head'));
+		$this->assertEquals($expectedBody, $this->doc->getJsScripts('body'));
+		
+		/* try to get from a location nknown not to exist */
+		$this->assertEquals(array(), $this->doc->getJsScripts('no-loc'));
+		$this->assertSame(
+			$this->doc,
+			$this->doc->addJsScript($tag, 'no-loc'),
+			'uses fluent interface'
+		);
+		$this->assertEquals(array(), $this->doc->getJsScripts('no-loc'));
+	}
+
+	/**
+	 * @return null
+	 */
+	public function testIsEnableDisableJs()
+	{
+		/* default setting is true */
+		$this->assertTrue($this->doc->isJs());
+
+		$this->assertSame(
+			$this->doc,
+			$this->doc->disableJs(),
+			'uses fluent interface'
+		);
+		$this->assertFalse($this->doc->isJs());
+	
+		$this->assertSame(
+			$this->doc,
+			$this->doc->enableJs(),
+			'uses fluent interface'
+		);
+
+		$this->assertTrue($this->doc->isJs());
+	}
+
+	/**
+	 * @return null
+	 */
+	public function testGetSetInlineJs()
+	{
+		/* default values are null */
+		$this->assertNull($this->doc->getInlineJs('head'));
+		$this->assertNull($this->doc->getInlineJs('body'));
+
+		$tag = $this->getMockBuilder('\Appfuel\View\Html\Element\Script')
+					  ->disableOriginalConstructor()
+					  ->getMock();
+
+		$this->assertSame(
+			$this->doc,
+			$this->doc->setInlineJs($tag, 'head'),
+			'uses fluent interface'
+		);
+
+		$this->assertSame($tag, $this->doc->getInlineJs('head'));
+		$this->assertNull($this->doc->getInlineJs('body'));
+
+		$tag2 = $this->getMockBuilder('\Appfuel\View\Html\Element\Script')
+					 ->disableOriginalConstructor()
+					 ->getMock();
+
+		$this->assertSame(
+			$this->doc,
+			$this->doc->setInlineJs($tag2, 'body'),
+			'uses fluent interface'
+		);
+
+		$this->assertSame($tag, $this->doc->getInlineJs('head'));
+		$this->assertSame($tag2, $this->doc->getInlineJs('body'));
+
+		/* try to get from a location nknown not to exist */
+		$this->assertEquals(null, $this->doc->getInlineJs('no-loc'));
+		$this->assertSame(
+			$this->doc,
+			$this->doc->setInlineJs($tag, 'no-loc'),
+			'uses fluent interface'
+		);
+		$this->assertEquals(null, $this->doc->getInlineJs('no-loc'));
+	}
+
+
+	/**
+	 * @return null
+	 */
+	public function testIsEnableDisableInlineJs()
+	{
+		/* default setting is true */
+		$this->assertTrue($this->doc->isInlineJs());
+
+		$this->assertSame(
+			$this->doc,
+			$this->doc->disableInlineJs(),
+			'uses fluent interface'
+		);
+		$this->assertFalse($this->doc->isInlineJs());
+	
+		$this->assertSame(
+			$this->doc,
+			$this->doc->enableInlineJs(),
+			'uses fluent interface'
+		);
+
+		$this->assertTrue($this->doc->isInlineJs());
+	}
+}
