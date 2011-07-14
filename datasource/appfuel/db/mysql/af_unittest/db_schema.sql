6d8109f70b70f355386ba85e9e6c2a16ce4dbe5f
Robert Scott-Buccluech <rsb.code@gmail.com>
2011-07-14 14:38:11 -0700

    removing db files not needed

--- datasource/appfuel/db/mysql/af_unittest/db_schema.sql
+++
@@ -1,3 +0,0 @@
--- af_unittest is used by appfuel unit tests to test 
--- appfuel's ability to interface with mysql. 
-create database af_unittest character set utf8 collate utf8_unicode_ci;
--- datasource/appfuel/db/mysql/af_unittest/test_queries/tables/data/test_queries.sql
+++
@@ -1,4 +0,0 @@
-insert into test_queries (query_id, param_1, param_2, param_3, result) values 
-(1, 1, 'code_a', 0, 'query issued'),
-(2, 0, 'code_b', 1, 'query 2 issued'),
-(3, 1, 'code_c', 2, 'query 3 issued');
