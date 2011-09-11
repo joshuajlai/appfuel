098ce96e2af9d8c7c087c82374f8cbaafa948885
Robert Scott-Buccluech <rsb.code@gmail.com>
2011-09-11 09:33:27 -0700

    finished test for http status

--- lib/TestFuel/Test/Http/HttpResponseStatusTest.php
+++ lib/TestFuel/Test/Http/HttpResponseStatusTest.php
@@ -51,4 +51,152 @@ class HttpResponseStatusTest extends BaseTestCase
 			$this->status
 		);
 	}
+
+	/**
+	 * Test that all codes are between 100 and 600 and all values and non
+	 * empty strings
+	 *
+	 * @return	array
+	 */
+	public function testStatusMap()
+	{
+		$map = $this->status->getStatusMap();
+		$this->assertInternalType('array', $map);
+		$this->assertNotEmpty($map);
+
+		foreach ($map as $code => $text) {
+			$this->assertGreaterThanOrEqual(100, $code);
+			$this->assertLessThan(600, $code);
+
+			$this->assertNotEmpty($text);
+			$this->assertInternalType('string', $text);
+		}
+	}
+
+	/**
+	 * Setup instantiated a new HttpResponseStatus with no paramaters.
+	 * This should default with code 200 and text 'ok'
+	 *
+	 * @return	null
+	 */
+	public function testDefaults()
+	{
+		$this->assertEquals(200, $this->status->getCode());
+		$this->assertEquals('OK',$this->status->getText());
+	}
+
+	/**
+	 * In this test we will get the status map and create new status objects
+	 * with only the code and test that the correct text was set
+	 *
+	 * @depends	testStatusMap
+	 * @return	null
+	 */
+	public function testAllStatusMapCodes()
+	{
+		$map = $this->status->getStatusMap();
+		foreach ($map as $code => $text) {
+			$status = new HttpResponseStatus($code);
+			$this->assertEquals($code, $status->getCode());
+			$this->assertEquals($text, $status->getText());
+		}
+	}
+
+	/**
+	 * This will test the ability to manually supply you own text for the 
+	 * code
+	 * 
+	 * @return	null
+	 */
+	public function testManualCodeOverride()
+	{
+		$code = 200;
+		$text = 'my own text';
+		$status = new HttpResponseStatus($code, $text);
+		$this->assertEquals($code, $status->getCode());
+		$this->assertEquals($text, $status->getText());
+	}
+
+	/**
+	 * @expectedException	Appfuel\Framework\Exception
+	 * @return	null
+	 */
+	public function testCodeIsNotAnInt_EmptyStringFailure()
+	{
+		$status = new HttpResponseStatus('');
+	}
+
+	/**
+	 * @expectedException	Appfuel\Framework\Exception
+	 * @return	null
+	 */
+	public function testCodeIsNotAnInt_NonEmptyStringFailure()
+	{
+		$status = new HttpResponseStatus('Abc');
+	}
+
+	/**
+	 * @expectedException	Appfuel\Framework\Exception
+	 * @return	null
+	 */
+	public function testCodeIsNotAnInt_ArrayFailure()
+	{
+		$status = new HttpResponseStatus(array(1,2,3));
+	}
+
+	/**
+	 * @expectedException	Appfuel\Framework\Exception
+	 * @return	null
+	 */
+	public function testCodeIsNotAnInt_ObjectFailure()
+	{
+		$status = new HttpResponseStatus(new StdClass());
+	}
+
+	/**
+	 * @expectedException	Appfuel\Framework\Exception
+	 * @return	null
+	 */
+	public function testCodeIsNotAnInt_FloadFailure()
+	{
+		$status = new HttpResponseStatus(103.233);
+	}
+
+	/**
+	 * @expectedException	Appfuel\Framework\Exception
+	 * @return	null
+	 */
+	public function testCodeIntLessThan100_ZeroFailure()
+	{
+		$status = new HttpResponseStatus(0);
+	}
+
+	/**
+	 * @expectedException	Appfuel\Framework\Exception
+	 * @return	null
+	 */
+	public function testCodeIntLessThan100_Failure()
+	{
+		$status = new HttpResponseStatus(99);
+	}
+
+	/**
+	 * @expectedException	Appfuel\Framework\Exception
+	 * @return	null
+	 */
+	public function testCodeIntGreaterThan600_Failure()
+	{
+		$status = new HttpResponseStatus(700);
+	}
+
+	/**
+	 * @expectedException	Appfuel\Framework\Exception
+	 * @return	null
+	 */
+	public function testCodeIntEqual600_Failure()
+	{
+		$status = new HttpResponseStatus(600);
+	}
+
+
 }
