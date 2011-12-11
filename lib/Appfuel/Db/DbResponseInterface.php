08b97976c364056b5445fcdba4fe3d4621c6eae3
Robert Scott-Buccluech <rsb.code@gmail.com>
2011-12-11 11:42:26 -0800

    adding comments to the DbResponseInterface

--- lib/Appfuel/Db/DbResponseInterface.php
+++ lib/Appfuel/Db/DbResponseInterface.php
@@ -15,8 +15,9 @@ use Iterator,
 
 /**
  * This interface is an vendor agnostic object used to encapsulate the
- * response of database execute from the vendor adapter which is 
- * is run by the DbHandler.
+ * resultset returned from database execution. This Object also delegates
+ * all errors to the Appfuel\Error\ErrorStack. It is encouraged to pass
+ * the error stack through the constructor making it immutable.
  */
 interface DbResponseInterface extends Iterator, Countable
 {
@@ -40,13 +41,45 @@ interface DbResponseInterface extends Iterator, Countable
 	public function getError();
 
 	/**
-	 * Determines if any errors are in the error stack
+	 * Determines if any errors are in the error stack.
 	 *
 	 * @return	bool
 	 */	
 	public function isError();
 
+	/**
+	 * @return	array
+	 */
 	public function getResultSet();
+
+	/**
+	 * Manually set the resultset. The resultset is the array arrays
+	 * with key value key => values pairs that is returned from the
+	 * database. If the vendor adapter supports multi queries then the 
+	 * resultset will be an array of DbResponseInterfaces each one 
+	 * representing that queries resultset.
+	 *
+	 * @param	array	$result
+	 * @return	DbResponseInterface
+	 */
 	public function setResultSet(array $results);
-	public function addResult($results);
+
+	/**
+	 * Allows you to add a result to the result set.
+	 *
+	 * Requirements:
+	 * 1) $result must be an array or DbResponseInterface
+	 *	  It is an InvalidArgumentException	for result to be anythingelse
+	 * 2) If key is not supplied or null than key will take the integer of
+	 *	  DbResponseInterface::count(). 
+	 * 3) Key must be an integer or a non empty string 
+	 *	  It is an InvalidArgumentException for key to be anythingelse
+	 * 4) If $key is a string then key must be trimed left and right
+	 *
+	 * @throws	InvalidArgumentException
+	 * @param	mixed array|DbResponseInterface $result
+	 * @param	mixed int|string $key
+	 * @return	DbResponseInterface
+	 */
+	public function addResult($result, $key = null);
 }
