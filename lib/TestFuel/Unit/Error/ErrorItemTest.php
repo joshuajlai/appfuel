395dc4a5c5a2ad7e3064ffe9a863ee5178e410a3
Robert Scott-Buccluech <rsb.code@gmail.com>
2011-12-13 14:14:48 -0800

    fix log module

--- lib/TestFuel/Unit/Error/ErrorItemTest.php
+++ lib/TestFuel/Unit/Error/ErrorItemTest.php
@@ -8,7 +8,7 @@
  * @copyright   2009-2010 Robert Scott-Buccleuch <rsb.code@gmail.com>
  * @license     http://www.apache.org/licenses/LICENSE-2.0
  */
-namespace TestFuel\Test\Error;
+namespace TestFuel\Unit\Error;
 
 use StdClass,
 	SplFileInfo,
