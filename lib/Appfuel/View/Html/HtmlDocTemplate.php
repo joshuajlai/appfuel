8fd3a6eac9303a8964d3ac3e38497a8d11bed756
Robert Scott-Buccluech <rsb.code@gmail.com>
2011-12-21 15:12:56 -0800

    wrong file path in last commit

--- lib/Appfuel/View/Html/HtmlDocTemplate.php
+++ lib/Appfuel/View/Html/HtmlDocTemplate.php
@@ -116,7 +116,7 @@ class HtmlDocTemplate extends ViewTemplate implements HtmlDocTemplateInterface
 								array $data = null)
 	{
 		if (null === $filePath) {
-			$filePath = 'appfuel/html/htmldoc.phtml';
+			$filePath = 'appfuel/htmldoc.phtml';
 		}
 
 		if (null === $compositor) {
