e47bdb1824354eabb518f0f1151b86001b69d322
Robert Scott-Buccluech <rsb.code@gmail.com>
2012-01-09 07:03:19 -0800

    finished uri, input and most of the acl codes for building a context

--- lib/Appfuel/Kernel/Mvc/MvcActionBuilder.php
+++ lib/Appfuel/Kernel/Mvc/MvcActionBuilder.php
@@ -43,6 +43,18 @@ class MvcActionBuilder implements MvcActionBuilderInterface
     protected $uri = null;
 
     /**
+     * Holds the input from the user request
+     * @var AppInputInterface
+     */
+    protected $input = null;
+
+    /**
+     * Acl role codes added to the context and used by the mvc action 
+     * @var array
+     */
+    protected $aclCodes = array();
+
+    /**
      * @param   string  $controllerClass
      * @return  MvcActionBuilder
      */
@@ -130,13 +142,13 @@ class MvcActionBuilder implements MvcActionBuilderInterface
      * Use the uri string from the server super global $_SERVER['REQUEST_URI']
      * to create the uri and set it
      *
-     * @return  ContextBuilder
+     * @return  MvcActionBuilder
      */
     public function useServerRequestUri()
     {
         $err  = 'php super global $_SERVER[\'REQUEST_URI\'] is not set';
         if (! isset($_SERVER['REQUEST_URI'])) {
-            throw new RunTimeException($err);
+            throw new LogicException($err);
         }
 
         $uri = $_SERVER['REQUEST_URI'];
@@ -158,5 +170,138 @@ class MvcActionBuilder implements MvcActionBuilderInterface
         return new RequestUri($uriString);
     }
 
+    /**
+     * @return  AppInputInterface
+     */
+    public function getInput()
+    {  
+        return $this->input;
+    }
 
+    /**
+     * @param   AppInputInterface   $input
+     * @return  MvcActionBuilder
+     */
+    public function setInput(AppInputInterface $input)
+    {
+        $this->input = $input;
+        return $this;
+    }
+
+    /**
+     * @param   string  $method 
+     * @param   array   $params 
+     * @return  ContextInput
+     */
+    public function createInput($method, array $params = array())
+    {
+        return new AppInput($method, $params);
+    }
+
+    /**
+     * By default we will use the parameters from the uri object, 
+     * super global $_POST for post, super global $_FILES for any files
+     * super global $_COOKIE for any cookies and super global $_SERVER['argv']
+     * for any commandline parameters.
+     *
+     * @return  ContextBuilder
+     */
+    public function buildInputFromDefaults($useUri = true)
+    {
+        $method = 'cli';
+        if (isset($_SERVER['REQUEST_METHOD'])) {
+            $method = $_SERVER['REQUEST_METHOD'];
+        }
+
+        if (empty($method) || ! is_string($method)) {
+            $err = 'request method is empty or not a string';
+            throw new RunTimeException($err);
+        }
+
+        if ($useUri) {
+            $uri = $this->getUri();
+            if (! $uri instanceof RequestUriInterface) {
+                if (isset($_SERVER['REQUEST_URI'])) {
+                    $uri = $this->useServerRequestUri()
+                                ->getUri();
+                }
+                else {
+                    $err  = 'Default get params come from the request uri.';
+                    $err .= 'we can not build the request uri without a uri ';
+                    $err .= 'string. Since no uri was given we looked for ';
+                    $err .= 'the uri string in $_SERVER[REQUEST_URI] and ';
+                    $err .= 'found it was not set.Please manually set super ';
+                    $err .= 'global or use builder to manually configure uri ';
+                    $err .= 'with method setUri';
+                    throw new RunTimeException($err);
+                }
+            }
+            $getParams = $uri->getParams();
+        } else {
+            $getParams = $_GET;
+        }
+
+        $params = array();
+        $params['get']    = $getParams;
+        $params['post']   = $_POST;
+        $params['files']  = $_FILES;
+        $params['cookie'] = $_COOKIE;
+        $params['argv']   = array();
+        if (isset($_SERVER['argv']) && is_array($_SERVER['argv'])) {
+            $params['argv'] = $_SERVER['argv'];
+        }
+
+        return $this->setInput($this->createInput($method, $params));
+    }
+
+    /** 
+     * @param   string  $method
+     * @param   array   $params
+     * @return  MvcActionBuilder
+     */
+    public function defineInputAs($method, array $params = array())
+    {
+        return $this->setInput($this->createInput($method, $params));
+    }
+
+    /**
+     * @param   string  $code
+     * @return  MvcActionDispatcher
+     */
+    public function addAclCode($code)
+    {
+        if (! is_string($code) || empty($code)) {
+            $err = 'role code must be a non empty string';
+            throw new InvalidArgumentException($err);
+        }
+
+        /* no duplicates */
+        if (in_array($code, $this->aclCodes)) {
+            return $this;
+        }
+
+        $this->aclCodes[] = $code;
+        return $this;
+    }
+
+    /**
+     * @param   array   $list
+     * @return  MvcActionDispatcher
+     */
+    public function addAclCodes(array $list)
+    {
+        foreach ($list as $code) {
+            $this->addAclCode($code);
+        }
+
+        return $this;
+    }
+
+	/**
+	 * @return	array
+	 */
+	public function getAclCodes()
+	{
+		return $this->aclCodes;
+	}
 }
--- lib/TestFuel/Unit/Kernel/Mvc/MvcActionBuilderTest.php
+++ lib/TestFuel/Unit/Kernel/Mvc/MvcActionBuilderTest.php
@@ -12,6 +12,7 @@ namespace TestFuel\Unit\Kernel\Mvc;
 
 use StdClass,
 	TestFuel\TestCase\BaseTestCase,
+	Appfuel\Kernel\Mvc\RequestUri,
 	Appfuel\Kernel\Mvc\MvcRouteDetail,
 	Appfuel\Kernel\Mvc\MvcActionBuilder;
 
@@ -29,12 +30,18 @@ class MvcActionBuilderTest extends BaseTestCase
 	 * @var string
 	 */
 	protected $actionClassName = null;
+
+    /**
+     * @var array
+     */
+    protected $serverBk = null;
 	
 	/**
 	 * @return null
 	 */
 	public function setUp()
 	{
+		$this->serverBk = $_SERVER;
 		$this->actionClassName = MvcActionBuilder::getActionClassName();
 		$this->builder = new MvcActionBuilder;
 	}
@@ -49,6 +56,7 @@ class MvcActionBuilderTest extends BaseTestCase
 		$name = $this->actionClassName;
 		$this->actionClassName = MvcActionBuilder::setActionClassName($name);
 		$this->builder = null;
+		$_SERVER = $this->serverBk;
 	}
 
 	/**
@@ -152,4 +160,501 @@ class MvcActionBuilderTest extends BaseTestCase
 		$uri = $this->builder->createUri($uriStr);
 		$this->assertInstanceOf('Appfuel\Kernel\Mvc\RequestUri', $uri);
 	}
+
+    /**
+     * @depends testSetUriWithString
+     * @return  null
+     */
+    public function testUseServerRequestUri()
+    {  
+        $uriString = 'my-route/param1/value1';
+        $_SERVER['REQUEST_URI'] = $uriString;
+
+        $this->assertSame(
+            $this->builder,
+            $this->builder->useServerRequestUri(),
+            'uses fluent interface'
+        );
+
+        $uri = $this->builder->getUri();
+        $this->assertInstanceOf(
+            'Appfuel\Kernel\Mvc\RequestUriInterface',
+            $uri,
+            'Uri object built from the SERVER[REQUEST_URI]'
+        );
+
+        $this->assertEquals($uriString, $uri->getUriString());
+    }
+
+    /**
+	 * @expectedException	LogicException
+     * @depends				testSetUriWithString
+     * @return				null
+     */
+	public function testUseServerRequestUriServerNotFound_Failure()
+	{
+		unset($_SERVER['REQUEST_URI']);
+        $this->builder->useServerRequestUri();
+	}
+
+    /**
+	 * @expectedException	LogicException
+	 * @dataProvider		provideInvalidStringsIncludeNull
+     * @depends				testSetUriWithString
+     * @return				null
+     */
+	public function testUseServerRequestUriUriNotString_Failure($uri)
+	{
+		$_SERVER['REQUEST_URI'] = $uri;
+        $this->builder->useServerRequestUri();
+	}
+
+    /**
+     * @depends testInitialState
+     * @return  null
+     */
+    public function testGetSetInput()
+    {
+        $this->assertNull($this->builder->getInput(), 'default value is null');
+
+        $input = $this->getMock('Appfuel\Kernel\Mvc\AppInputInterface');
+        $this->assertSame(
+            $this->builder,
+            $this->builder->setInput($input),
+            'uses fluent interface'
+        );
+        $this->assertSame($input, $this->builder->getInput());
+    }
+
+    /**
+     * @depends testInitialState
+     * @return  null
+     */
+    public function testCreateInput()
+    {
+        $inputClass = 'Appfuel\Kernel\Mvc\AppInput';
+        $input = $this->builder->createInput('get');
+        $expected = array(
+            'get'   => array(),
+            'post'  => array(),
+            'files' => array(),
+            'cookie' => array(),
+            'argv'  => array()
+        );
+        $this->assertEquals($expected, $input->getAll());
+        $this->assertInstanceOf($inputClass, $input);
+
+        $input = $this->builder->createInput('post');
+        $this->assertInstanceOf($inputClass, $input);
+        $this->assertEquals($expected, $input->getAll());
+
+        $input = $this->builder->createInput('cli');
+        $this->assertInstanceOf($inputClass, $input);
+        $this->assertEquals($expected, $input->getAll());
+
+
+        $params = array(
+            'get'    => array('param1' => 'value1'),
+            'post'   => array('param2' => 'value2'),
+            'files'  => array('param3' => 'value3'),
+            'cookie' => array('param4' => 'value4'),
+            'argv'   => array('param5' => 'value5')
+        );
+        $input = $this->builder->createInput('get', $params);
+        $this->assertInstanceOf($inputClass, $input);
+        $this->assertEquals($params, $input->getAll());
+
+        $input = $this->builder->createInput('post', $params);
+        $this->assertInstanceOf($inputClass, $input);
+        $this->assertEquals($params, $input->getAll());
+
+        $input = $this->builder->createInput('cli', $params);
+        $this->assertInstanceOf($inputClass, $input);
+        $this->assertEquals($params, $input->getAll());
+    }
+
+    /**
+     * When the request method is not set it will default to cli. This is 
+     * because php automatically sets the REQUEST_METHOD in the server super
+     * global for all http request but not for cli request. 
+     * 
+     * @depends testCreateInput
+     * @return  null
+     */
+    public function testBuildInputFromDefaultsRequestMethodNotSet()
+    {
+        $this->assertNull($this->builder->getInput());
+
+        $uri = new RequestUri('my-route/af-param1/value1');
+        $this->builder->setUri($uri);
+
+        $this->assertSame(
+            $this->builder,
+            $this->builder->buildInputFromDefaults(),
+            'uses fluent interface'
+        );
+    
+        $input = $this->builder->getInput();
+        $this->assertInstanceOf(
+            'Appfuel\Kernel\Mvc\AppInput',
+            $input
+        );
+
+        $expected = array(
+            'get'    => array('af-param1' => 'value1'),
+            'post'   => $_POST,
+            'files'  => $_FILES,
+            'cookie' => $_COOKIE,
+            'argv'   => $_SERVER['argv']
+        );
+
+        $this->assertEquals($expected, $input->getAll());
+    }
+
+    /**
+     * @depends testCreateInput
+     * @return  null
+     */
+    public function testDefineInputAsNoParams()
+    {
+        $this->assertNull($this->builder->getInput());
+
+        $emptyParams = array(
+            'get'    => array(),
+            'post'   => array(),
+            'files'  => array(),
+            'cookie' => array(),
+            'argv'   => array()
+        );
+
+
+        $method = 'get';
+        $this->assertSame(
+            $this->builder,
+            $this->builder->defineInputAs($method),
+            'uses fluent interface'
+        );
+    
+        $input = $this->builder->getInput();
+        $this->assertInstanceOf(
+            'Appfuel\Kernel\Mvc\AppInput',
+            $input
+        );
+
+        $this->assertEquals($method, $input->getMethod());
+        $this->assertEquals($emptyParams, $input->getAll());
+
+        $method = 'post';
+        $this->assertSame(
+            $this->builder,
+            $this->builder->defineInputAs($method),
+            'uses fluent interface'
+        );
+    
+        $input = $this->builder->getInput();
+        $this->assertInstanceOf(
+            'Appfuel\Kernel\Mvc\AppInput',
+            $input
+        );
+
+
+        $this->assertEquals($method, $input->getMethod());
+        $this->assertEquals($emptyParams, $input->getAll());
+
+        $method = 'cli';
+        $this->assertSame(
+            $this->builder,
+            $this->builder->defineInputAs($method),
+            'uses fluent interface'
+        );
+
+        $input = $this->builder->getInput();
+        $this->assertInstanceOf(
+            'Appfuel\Kernel\Mvc\AppInput',
+            $input
+        );
+
+        $this->assertEquals($method, $input->getMethod());
+        $this->assertEquals($emptyParams, $input->getAll());
+    }
+
+
+    /**
+     * @depends testCreateInput
+     * @return  null
+     */
+    public function testDefineInputAsWithGetParams()
+    {
+        $this->assertNull($this->builder->getInput());
+
+        $params = array(
+            'get' => array('param1' => 'value1'),
+        );
+        $method = 'get';
+        $this->assertSame(
+            $this->builder,
+            $this->builder->defineInputAs($method, $params),
+            'uses fluent interface'
+        );
+    
+        $input = $this->builder->getInput();
+        $this->assertInstanceOf(
+            'Appfuel\Kernel\Mvc\AppInput',
+            $input
+        );
+
+        $expected = array(
+            'get'   => $params['get'],
+            'post'  => array(),
+            'files' => array(),
+            'cookie' => array(),
+            'argv'  => array()
+        );
+        $this->assertEquals($method, $input->getMethod());
+        $this->assertEquals($expected, $input->getAll());
+    }
+
+    /**
+     * @depends testCreateInput
+     * @return  null
+     */
+    public function testDefineInputAsWithPostParams()
+    {
+        $this->assertNull($this->builder->getInput());
+
+        $params = array(
+            'post' => array('param1' => 'value1'),
+        );
+        $method = 'post';
+        $this->assertSame(
+            $this->builder,
+            $this->builder->defineInputAs($method, $params),
+            'uses fluent interface'
+        );
+    
+        $input = $this->builder->getInput();
+        $this->assertInstanceOf(
+            'Appfuel\Kernel\Mvc\AppInput',
+            $input
+        );
+
+        $expected = array(
+            'get'   => array(),
+            'post'  => $params['post'],
+            'files' => array(),
+            'cookie' => array(),
+            'argv'  => array()
+        );
+        $this->assertEquals($method, $input->getMethod());
+        $this->assertEquals($expected, $input->getAll());
+    }
+
+    /**
+     * @depends testCreateInput
+     * @return  null
+     */
+    public function testDefineInputAsWithFilesParams()
+    {
+        $this->assertNull($this->builder->getInput());
+
+        $params = array(
+            'files' => array('param1' => 'value1'),
+        );
+        $method = 'cli';
+        $this->assertSame(
+            $this->builder,
+            $this->builder->defineInputAs($method, $params),
+            'uses fluent interface'
+        );
+    
+        $input = $this->builder->getInput();
+        $this->assertInstanceOf(
+            'Appfuel\Kernel\Mvc\AppInput',
+            $input
+        );
+
+        $expected = array(
+            'get'   => array(),
+            'post'  => array(),
+            'files' => $params['files'],
+            'cookie' => array(),
+            'argv'  => array()
+        );
+        $this->assertEquals($method, $input->getMethod());
+        $this->assertEquals($expected, $input->getAll());
+    }
+
+    /**
+     * @depends testCreateInput
+     * @return  null
+     */
+    public function testDefineInputAsWithCookieParams()
+    {
+        $this->assertNull($this->builder->getInput());
+
+        $params = array(
+            'cookie' => array('param1' => 'value1'),
+        );
+        $method = 'get';
+        $this->assertSame(
+            $this->builder,
+            $this->builder->defineInputAs($method, $params),
+            'uses fluent interface'
+        );
+    
+        $input = $this->builder->getInput();
+        $this->assertInstanceOf(
+            'Appfuel\Kernel\Mvc\AppInput',
+            $input
+        );
+
+        $expected = array(
+            'get'   => array(),
+            'post'  => array(),
+            'files' => array(),
+            'cookie' => $params['cookie'],
+            'argv'  => array()
+        );
+        $this->assertEquals($method, $input->getMethod());
+        $this->assertEquals($expected, $input->getAll());
+    }
+
+    /**
+     * @depends testCreateInput
+     * @return  null
+     */
+    public function testDefineInputAsWithArgvParams()
+    {
+        $this->assertNull($this->builder->getInput());
+
+        $params = array(
+            'argv' => array('param1' => 'value1'),
+        );
+        $method = 'cli';
+        $this->assertSame(
+            $this->builder,
+            $this->builder->defineInputAs($method, $params),
+            'uses fluent interface'
+        );
+    
+        $input = $this->builder->getInput();
+        $this->assertInstanceOf(
+            'Appfuel\Kernel\Mvc\AppInput',
+            $input
+        );
+
+        $expected = array(
+            'get'   => array(),
+            'post'  => array(),
+            'files' => array(),
+            'cookie' => array(),
+            'argv'  => $params['argv']
+        );
+        $this->assertEquals($method, $input->getMethod());
+        $this->assertEquals($expected, $input->getAll());
+    }
+
+    /**
+     * @depends testCreateInput
+     * @return  null
+     */
+    public function testDefineInputAsWithAllParams()
+    {
+        $this->assertNull($this->builder->getInput());
+
+        $params = array(
+            'get' => array('param1' => 'value1'),
+            'post' => array('paramr2' => 'value2'),
+            'files' => array('param3' => 'value3'),
+            'cookie' => array('param4' => 'value4'),
+            'argv' => array('param5' => 'value5'),
+        );
+        $method = 'cli';
+        $this->assertSame(
+            $this->builder,
+            $this->builder->defineInputAs($method, $params),
+            'uses fluent interface'
+        );
+    
+        $input = $this->builder->getInput();
+        $this->assertInstanceOf(
+            'Appfuel\Kernel\Mvc\AppInput',
+            $input
+        );
+
+        $this->assertEquals($method, $input->getMethod());
+        $this->assertEquals($params, $input->getAll());
+    }
+
+	/**
+	 * @depends	testInitialState
+	 * @return	null
+	 */
+	public function testGetAddAclCode()
+	{
+		$this->assertEquals(array(), $this->builder->getAclCodes());
+		
+		$code1 = 'my-code';
+		$this->assertSame(
+			$this->builder,
+			$this->builder->addAclCode($code1)
+		);
+		$expected = array($code1);
+		$this->assertEquals($expected, $this->builder->getAclCodes());
+
+		$code2 = 'your-code';
+		$this->assertSame(
+			$this->builder,
+			$this->builder->addAclCode($code2)
+		);
+		$expected = array($code1, $code2);
+		$this->assertEquals($expected, $this->builder->getAclCodes());
+
+		$code3 = 'our-code';
+		$this->assertSame(
+			$this->builder,
+			$this->builder->addAclCode($code3)
+		);
+		$expected = array($code1, $code2, $code3);
+		$this->assertEquals($expected, $this->builder->getAclCodes());
+	}
+
+	/**
+	 * @depends	testGetAddAclCode
+	 * @return	null
+	 */
+	public function testAddAclCodeNoDuplicates()
+	{
+		$code1 = 'my-code';
+		$code2 = 'my-code';
+		$code3 = 'my-code';
+		$this->builder->addAclCode($code1)
+					  ->addAclCode($code2)
+					  ->addAclCode($code3);
+
+		$this->assertEquals(array($code1), $this->builder->getAclCodes());
+	}
+
+	/**
+	 * @expectedException	InvalidArgumentException
+	 * @depends				testGetAddAclCode
+	 * @return				null
+	 */
+	public function testAddAclCodeEmptyString_Failure()
+	{
+		$this->builder->addAclCode('');
+	}
+
+	/**
+	 * @expectedException	InvalidArgumentException
+	 * @dataProvider		provideInvalidStringsIncludeNull
+	 * @depends				testGetAddAclCode
+	 * @return				null
+	 */
+	public function testAddAclCodeInvalidString_Failure($code)
+	{
+		$this->builder->addAclCode($code);
+	}
+
+
 }
