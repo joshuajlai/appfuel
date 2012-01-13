f078a49d0b72e5407a040ead78e219cfe4e3bf56
Robert Scott-Buccluech <rsb.code@gmail.com>
2012-01-13 10:21:03 -0800

    adding kernel content object and reverting back the context build to be backwards compatible

---
+++ lib/Appfuel/Kernel/ContentInterface.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * Appfuel
+ * PHP 5.3+ object oriented MVC framework supporting domain driven design. 
+ *
+ * @package     Appfuel
+ * @author      Robert Scott-Buccleuch <rsb.code@gmail.com.com>
+ * @copyright   2009-2010 Robert Scott-Buccleuch <rsb.code@gmail.com>
+ * @license     http://www.apache.org/licenses/LICENSE-2.0
+ */
+namespace Appfuel\Kernel;
+
+/**
+ * Holds generic blocks of string in an array to be constructed later into
+ * a single string
+ */
+interface ContentInterface
+{
+	/**
+	 * @return string
+	 */
+	public function getSeparator();
+
+	/**
+	 * Used during content building to separate each block of content 
+	 *
+	 * Requirements:
+	 * 1) it is a InvalidArgumentException for $char to non scalar
+	 * 2) return ContentInterface
+	 *
+	 * @throws	InvalidArgumentException
+	 * @param	scalar	$char
+	 * @return	ContentInterface
+	 */
+	public function setSeparator($char);
+
+	/**
+	 * Add a content block to the content list. Each block will 
+	 * be concatenated togather with the content sepearator during build.
+	 *
+	 * Requirements:
+	 * 1) $data must be a scalar value of an object that implements __toString
+	 *    you must throw an InvalidArgumentException otherwise. This helps
+	 *	  keep TagContentInterface::build simple
+	 * 2) $action must be a non empty string. 
+	 *    you must throw an InvalidArgumentException otherwise
+	 * 3) you must normalize $action to be all lower case
+	 * 4) if action is not (append|prepend|replace) 
+	 *	  you must throw an InvalidArgumentException
+	 *    append  - must add the content block to the end of the array
+	 *    prepend - must add the content block to the beginning of the array
+	 *    replace - must replace the content with an array of one which is 
+	 *				the content block given
+	 *	 
+	 * 5) regardless of the action you must convert the content block to a 
+	 *    a string during the assignment
+	 * 6) return TagContentInterface
+	 * 
+	 * @param	mixed scalar|object	$data	
+	 * @param	string				$action  (append|prepend|replace)	
+	 * @return	ContentInterface
+	 */
+    public function add($data, $action = 'append');
+
+	/**
+	 * Load a series of content blocks at once.
+	 *
+	 * Requirements:
+	 * 1) when the item in the list is a string delagate call to
+	 *    TagContentInterface::add using default action
+	 * 2) when the item in the list is an array and both the first and second
+	 *    element are set then delegate to TagContentInterface::add where
+	 *	  element 1 is param 1 and element 2 is param 2.
+	 *    if the elements are not set the skip that iteration
+	 * 3) trate as a fluent interface and return TagContentInterface
+	 *
+	 * @param	array	$list
+	 * @return	ContentInterface
+	 */
+	public function load(array $list);
+
+	/**
+	 * Return a content block at a given index.
+	 *
+	 * Requirements:
+	 * 1) when index is null return all the content
+	 * 2) when index is not an integer or out of bounds return false
+	 *    otherwise return the content block at that index
+	 *
+	 * @param	int	 $index	default null
+	 * @return	string|false
+	 */
+	public function get($index = null);
+
+	/**
+	 * Requirements:
+	 * 1) return the number of content blocks added
+	 *
+	 * @return int
+	 */
+	public function count();
+
+	/**
+	 * Requirements:
+	 * 1) must return true when content count is 0 and false otherwise
+	 *
+	 * @return	bool
+	 */
+	public function isEmpty();
+
+	/**
+	 * Requirements:
+	 * 1) when index is null replace the content with an empty array
+	 * 2) when index is an int and in range unset that content block
+	 * 3) when index is not an integer or out of range return false
+	 *
+	 * @param	int	$index	default	null
+	 * @return	null | false when invalid index
+	 */
+	public function clear($index = null);
+
+	/**
+	 * Build the list of content blocks into a string
+	 *
+	 * Requirements:
+	 * 1) concatenate each content block with the character specified by
+	 *    TagContentInterface::getSeparator
+	 * 2) when no content exists return an empty string
+	 * 3) ensure any tailing separator characters has been trimed of the
+	 *    left and right side of the result
+	 * 4) ensure no exceptions are throw, must be used from __toString
+	 *
+	 * @return string
+	 */
+	public function build();
+
+	/**
+	 * Requirements:
+	 * 1) must call build
+	 *
+	 * @return string
+	 */
+	public function __toString();
+}
--- lib/Appfuel/Kernel/Mvc/ContextBuilder.php
+++ lib/Appfuel/Kernel/Mvc/ContextBuilder.php
@@ -11,10 +11,7 @@
 namespace Appfuel\Kernel\Mvc;
 
 use RunTimeException,
-	InvalidArgumentException
-	Appfuel\Kernel\KernelRegistry,
-    Appfuel\ClassLoader\StandardAutoLoader,
-    Appfuel\ClassLoader\AutoLoaderInterface;
+	InvalidArgumentException;
 
 /**
  * The context build holds all the logic for create uri strings, requests,
@@ -23,20 +20,6 @@ use RunTimeException,
  */
 class ContextBuilder implements ContextBuilderInterface
 {
-	/**
-	 * The class name used to create the action controller class found in
-	 * the namespace the route maps too
-	 * @var	string
-	 */
-	static protected $actionClassName = 'ActionController';
-
-	/**
-	 * We reuse the autoloader class to parse the namespace into a dir path
-	 * to find the mvc action, view, and route detail.
-	 * @var AutoLoaderInterface
-	 */
-	protected $loader = null;
-
     /**
      * Request Parameters. We parse the uri string and create our own parameters
      * instead of using super global $_GET. This is due to the way we use the 
@@ -52,290 +35,270 @@ class ContextBuilder implements ContextBuilderInterface
     protected $input = null;
 
     /**
-     * @param   string  $controllerClass
-     * @return  MvcActionBuilder
+     * @var string
      */
-    public function __construct(AutoLoaderInterface $loader = null)
+    protected $strategy = null;
+
+    /**
+     * @var string  
+     */
+    protected $route = null;
+
+    /**
+     * @return  string
+     */
+    public function getStrategy()
     {
-        /*
-         * Note that we use the load class from the lib directory. This 
-         * constant is set during intialization. I will refactor next to a 
-         * a path finder. (on a deadline right now) --rsb
-         */
-        if (null === $loader) {
-            $loader = new StandardAutoLoader(AF_LIB_PATH);
-        }
-        $this->setClassLoader($loader);
+        return $this->strategy;
     }
 
-	/**
-	 * @param	string	$name
-	 * @return	null
-	 */
-	static public function setActionClassName($name)
-	{
-		if (! is_string($name) || ! ($name = trim($name))) {
-			$err = 'class name must be a non empty string';
-			throw new InvalidArgumentException($err);
-		}
+    /**
+     * @param   string  $strategy
+     * @return  null
+     */
+    public function setStrategy($strategy)
+    {
+        if (empty($strategy) || ! is_string($strategy)) {
+            $err = 'strategy must be a non empty string';
+            throw new InvalidArgumentException($err);
+        }
+        $strategy = strtolower($strategy);
 
-		self::$actionClassName = $name;
-	}
+        $valid = array('html', 'html-page', 'console', 'ajax');
+        if (! in_array($strategy, $valid, true)) {
+            $err  = 'strategy must be on of the following ';
+            $err .= '-(' . implode('|', $valid) . ')';
+            throw new InvalidArgumentException($err);
+        }
+        $this->strategy = $strategy;
+        return $this;
+    }
 
-	/**
-	 * @return	string
-	 */
-	static public function getActionClassName()
-	{
-		return self::$actionClassName;
-	}
+    /**
+     * @return  string
+     */
+    public function getRoute()
+    {
+        return $this->route;
+    }
 
-	/**
-	 * @return	AutoLoaderInterface
-	 */
-	public function getClassLoader()
-	{
-		return $this->loader;
-	}
+    /**
+     * @param   string  
+     * @return  ContextBuilder
+     */
+    public function setRoute($route)
+    {
+        if (! is_string($route)) {
+            $err = 'the route for this context must be a string';
+            throw new InvalidArgumentException($err);
+        }
+        $this->route = $route;
+        return $this;
+    }
 
-	/**
-	 * @param	AutoLoaderInterface $loader
-	 * @return	MvcActionBuilder
-	 */
-	public function setClassLoader(AutoLoaderInterface $loader)
-	{
-		$this->loader $loader;
-		return $this;
-	}
+    /**
+     * @return  RequestUriInterface
+     */
+    public function getUri()
+    {
+        return $this->uri;
+    }
 
-	/**
-	 * @return	RequestUriInterface
-	 */
-	public function getUri()
-	{
-		return $this->uri;
-	}
+    /**
+     * @param   RequestUriInterface $uri
+     * @return  ContextBuilder
+     */
+    public function setUri(RequestUriInterface $uri)
+    {
+        $this->uri = $uri;
+        return $this;
+    }
 
-	/**
-	 * @param	RequestUriInterface	$uri
-	 * @return	ContextBuilder
-	 */
-	public function setUri(RequestUriInterface $uri)
-	{
-		$this->uri = $uri;
-		return $this;
-	}
-	
-	/**
-	 * @param	string	$uriString
-	 * @return	RequestUri
-	 */
-	public function createUri($uriString)
-	{
-		return new RequestUri($uriString);	
-	}
+    /**
+     * @param   string  $uriString
+     * @return  RequestUri
+     */
+    public function createUri($uriString)
+    {
+        return new RequestUri($uriString);
+    }
 
-	/**
-	 * Use the uri string from the server super global $_SERVER['REQUEST_URI']
-	 * to create the uri and set it
-	 *
-	 * @return	ContextBuilder
-	 */
-	public function useServerRequestUri()
-	{
-		$err  = 'ConextBuilder failed: php super global ';
-		$err .= '$_SERVER[\'REQUEST_URI\']';
-		if (! isset($_SERVER['REQUEST_URI'])) {
-			throw new RunTimeException("$err is not set");
-		}
-
-		$uri = $_SERVER['REQUEST_URI'];
-		if (! is_string($uri)) {
-			throw new RunTimeException("$err must be a valid string");
-		}
-		return $this->setUri($this->createUri($uri));
-	}
-
-	/**
-	 * @param	string	$uri
-	 * @return	ContextBuilder
-	 */
-	public function useUriString($uri)
-	{
-		if (! is_string($uri)) {
-			$err  = 'ConextBuilder failed: uri string given must be ';
-			$err .= 'a valid string';
-			throw new InvalidArgumentException($err);
-		}
-
-		return $this->setUri($this->createUri($uri));
-	}
-
-	/**
-	 * @return	AppInputInterface
-	 */
-	public function getInput()
-	{
-		return $this->input;
-	}
-
-	/**
-	 * @param	AppInputInterface	$input
-	 * @return	ContextBuilder
-	 */
-	public function setInput(AppInputInterface $input)
-	{
-		$this->input = $input;
-		return $this;
-	}
-
-	/**
-	 * By default we will use the parameters from the uri object, 
-	 * super global $_POST for post, super global $_FILES for any files
-	 * super global $_COOKIE for any cookies and super global $_SERVER['argv']
-	 * for any commandline parameters.
-	 *
-	 * @return	ContextBuilder
-	 */
-	public function buildInputFromDefaults($useUri = true)
-	{
-		$method = 'cli';
-		$err    = 'ContextBuilder failed:';
-		if (isset($_SERVER['REQUEST_METHOD'])) {
-			$method = $_SERVER['REQUEST_METHOD'];
-		}
-		
-		if (empty($method) || ! is_string($method)) {
-			$err .= 'request method is empty or not a string';
-			throw new RunTimeException($err);
-		}
-
-		if ($useUri) {
-			$uri = $this->getUri();
-			if (! $uri instanceof RequestUriInterface) {
-				if (isset($_SERVER['REQUEST_URI'])) {
-					$uri = $this->useServerRequestUri()
-								->getUri();
-				}
-				else {
-					$err .= 'Default get params come from the request uri.';
-					$err .= 'we can not build the request uri without a uri ';
-					$err .= 'string. Since no uri was given we looked for ';
-				    $err .= 'the uri string in $_SERVER[REQUEST_URI] and ';
-					$err .= 'found it was not set.Please manually set super ';
-					$err .= 'global or use builder to manually configure uri ';
-					$err .= 'with method useUriString';
-					throw new RunTimeException($err);
-				}
-			}
-			$getParams = $uri->getParams();
-		} else {
-			$getParams = $_GET;
-		}
-
-		$params = array();
-		$params['get']    = $getParams;
-		$params['post']   = $_POST;
-		$params['files']  = $_FILES;
-		$params['cookie'] = $_COOKIE;
-		$params['argv']   = array();
-		if (isset($_SERVER['argv']) && is_array($_SERVER['argv'])) {
-			$params['argv'] = $_SERVER['argv'];
-		}
-
-		return $this->setInput($this->createInput($method, $params));
-	}
-
-	/**
-     * This will allow you to manual define the input used in the context 
-     * that will be dispatched. If a uri has also been defined then its 
-     * parameters will be used as the inputi's get parameters by default. If
-     * you already have get parameters then the uri params will be merged
+    /**
+     * Use the uri string from the server super global $_SERVER['REQUEST_URI']
+     * to create the uri and set it
      *
-     * @param   string  $method  get|post or cli
-     * @param   array   $params  input parameters
-     * @param   bool    $useUri  flag used to determine if the get parameters
-     *                           will be obtained from the uri
-     * @return  MvcActionBuilder
-	 */
-	public function defineInput($method, array $params, $useUri = true)
-	{
-        if (true === $useUri) {
+     * @return  ContextBuilder
+     */
+    public function useServerRequestUri()
+    {
+        $err  = 'ConextBuilder failed: php super global ';
+        $err .= '$_SERVER[\'REQUEST_URI\']';
+        if (! isset($_SERVER['REQUEST_URI'])) {
+            throw new RunTimeException("$err is not set");
+        }
+
+        $uri = $_SERVER['REQUEST_URI'];
+        if (! is_string($uri)) {
+            throw new RunTimeException("$err must be a valid string");
+        }
+        return $this->setUri($this->createUri($uri));
+    }
+
+    /**
+     * @param   string  $uri
+     * @return  ContextBuilder
+     */
+    public function useUriString($uri)
+    {
+        if (! is_string($uri)) {
+            $err  = 'ConextBuilder failed: uri string given must be ';
+            $err .= 'a valid string';
+            throw new InvalidArgumentException($err);
+        }
+
+        return $this->setUri($this->createUri($uri));
+    }
+
+
+    /**
+     * @return  AppInputInterface
+     */
+    public function getInput()
+    {
+        return $this->input;
+    }
+
+    /**
+     * @param   AppInputInterface   $input
+     * @return  ContextBuilder
+     */
+    public function setInput(AppInputInterface $input)
+    {
+        $this->input = $input;
+        return $this;
+    }
+
+    /**
+     * By default we will use the parameters from the uri object, 
+     * super global $_POST for post, super global $_FILES for any files
+     * super global $_COOKIE for any cookies and super global $_SERVER['argv']
+     * for any commandline parameters.
+     *
+     * @return  ContextBuilder
+     */
+    public function buildInputFromDefaults($useUri = true)
+    {
+        $method = 'cli';
+        $err    = 'ContextBuilder failed:';
+        if (isset($_SERVER['REQUEST_METHOD'])) {
+            $method = $_SERVER['REQUEST_METHOD'];
+        }
+
+        if (empty($method) || ! is_string($method)) {
+            $err .= 'request method is empty or not a string';
+            throw new RunTimeException($err);
+        }
+
+        if ($useUri) {
             $uri = $this->getUri();
-            if (! ($uri instanceof RequestUriInterface)) {
-                $err  = "defineInput failed: uri is required for its get ";
-                $err .= "params, but has not been set";
-                throw new RunTimeException($err);
+            if (! $uri instanceof RequestUriInterface) {
+                if (isset($_SERVER['REQUEST_URI'])) {
+                    $uri = $this->useServerRequestUri()
+                                ->getUri();
+                }
+                else {
+                    $err .= 'Default get params come from the request uri.';
+                    $err .= 'we can not build the request uri without a uri ';
+                    $err .= 'string. Since no uri was given we looked for ';
+                    $err .= 'the uri string in $_SERVER[REQUEST_URI] and ';
+                    $err .= 'found it was not set.Please manually set super ';
+                    $err .= 'global or use builder to manually configure uri ';
+                    $err .= 'with method useUriString';
+                    throw new RunTimeException($err);
+                }
             }
             $getParams = $uri->getParams();
-            if (array_key_exists('get', $params)) {
-                $getParams = array_merge($params['get'], $getParams);
-            }
-            $params['get'] = $getParams;
+        } else {
+            $getParams = $_GET;
+        }
+
+        $params = array();
+        $params['get']    = $getParams;
+        $params['post']   = $_POST;
+        $params['files']  = $_FILES;
+        $params['cookie'] = $_COOKIE;
+        $params['argv']   = array();
+        if (isset($_SERVER['argv']) && is_array($_SERVER['argv'])) {
+            $params['argv'] = $_SERVER['argv'];
         }
 
         return $this->setInput($this->createInput($method, $params));
-	}
+    }
 
-	/**
-	 * @param	string	$method	
-	 * @param	array	$params	
-	 * @return	ContextInput
-	 */
-	public function createInput($method, array $params = array())
-	{
-		return new AppInput($method, $params);
-	}
+    /** 
+     * @param   string  $method
+     * @param   array   $params
+     * @return  ContextBuilder
+     */
+    public function defineInputAs($method, array $params = array())
+    {
+        return $this->setInput($this->createInput($method, $params));
+    }
 
-	/**
-	 * @return	AppContext
-	 */
-	public function build($routeKey, $strategy)
-	{
-		$err = 'context build failed: ';
-		$route  = $this->getRoute();
-		if (null === $route) {
-			$err .= 'route must be set before build';
-			throw new RunTimeException($err);
-		}
+    /**
+     * @param   string  $method 
+     * @param   array   $params 
+     * @return  ContextInput
+     */
+    public function createInput($method, array $params = array())
+    {
+        return new AppInput($method, $params);
+    }
+    /**
+     * @return  AppContext
+     */
+    public function build()
+    {
+        $err = 'context build failed: ';
+        $route  = $this->getRoute();
+        if (null === $route) {
+            $err .= 'route must be set before build';
+            throw new RunTimeException($err);
+        }
 
-		$strategy = $this->getStrategy();
-		if (null === $strategy) {
-			$err .= 'strategy must be set before build';
-			throw new RunTimeException($err);
-		}
+        $strategy = $this->getStrategy();
+        if (null === $strategy) {
+            $err .= 'strategy must be set before build';
+            throw new RunTimeException($err);
+        }
 
 
-		$input = $this->getInput();
-		if (! $input instanceof AppInputInterface) {
-			$uri = $this->getUri();
-			if (! $uri instanceof RequestUriInterface) {
-				$uri = $this->useServerRequestUri();
-			}
+        $input = $this->getInput();
+        if (! $input instanceof AppInputInterface) {
+            $uri = $this->getUri();
+            if (! $uri instanceof RequestUriInterface) {
+                $uri = $this->useServerRequestUri();
+            }
 
-			$input = $this->buildInputFromDefaults()
-						  ->getInput();
-		}
+            $input = $this->buildInputFromDefaults()
+                          ->getInput();
+        }
 
-		$this->clear();
+        $this->clear();
 
-		/* clear out build state */
-		return new MvcContext($route, $strategy, $input);
-	}
+        /* clear out build state */
+        return new MvcContext($route, $strategy, $input);
+    }
 
-	public function buildView($namespace)
-	{
-		
-	}
-
-	/**
-	 * @return	null
-	 */
-	protected function clear()
-	{
-		$this->route = null;
-		$this->strategy = null;
-		$this->input = null;
-		$this->uri   = null;
-	}
+    /**
+     * @return  null
+     */
+    protected function clear()
+    {
+        $this->route = null;
+        $this->strategy = null;
+        $this->input = null;
+        $this->uri   = null;
+    }
+}
 }
